c90043aae6c5de20b1c225cf9f3e1178f50a3bcd
Karina Matiolli <70562104+KaMatiolli@users.noreply.github.com>
2021-10-07 15:36:24 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+ printf("Olá, Planeta Terra!\n");
+ printf("Me chamo %s\n", "Karina");
+ printf("Sou do sexo %c\n", 'F');
+ printf("Tenho %.2fm de altura e %d anos\n", 1.74,2020-1996);
+ printf("O meu e-mail tem um %c após meu nome e o último sobrenome\n", '@');
+ printf("Estamos no ano mais louco, e só vivemos %f%%\n", 9.0/12.0);
+ 
+ printf("O valor de bool é %d\n", 5<10);
+ 
+
+  char endereco[101] = "Rua Catende, Jardim Nordeste, São Paulo/SP - 03691000";
+  printf("Endereço: %s\n", endereco);
+  char sexo = 'F';
+  printf("Você é do sexo %c\n", sexo);
+  char escolha = 'N';
+  printf("Opção escolhida: %c\n", escolha);
+
+  int divisao = 81/2;
+  printf("81/2 = %d\n", divisao);
+  int resultado = 3<9;
+  printf("3 é menor que 9? %d\n", resultado);
+  int guiche = 3;
+  printf("Dirija-se ao guiche %d\n", guiche);
+  int idade;
+
+
+  
+  float preco = 27.03;
+  printf("O produto custa R$%f\n", preco);
+  float media = (10+8)/2.0;
+  printf("Sua média é de %f\n", media);
+
+
+
+
+
+  
+
+ 
+
+
+ 
+}
